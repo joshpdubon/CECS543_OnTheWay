684a24a1d8249c7ef5c6b98ed9f916eba252bac4
Joshua Dubon <jdubon11@gmail.com>
2015-11-15 20:07:29 -0800

    Working towards completing main algorithm

--- InitMap.js
+++ InitMap.js
@@ -3,7 +3,7 @@ var map;
 var csulb = {lat: 33.783, lng: -118.114};
 var numPoints = 20;
 var result;
-var status;
+var searchRadius = 1000; // meters
 
 // Variables to manipulate the origin, destination, and stop-off points
 var autocomp_origin;
@@ -11,7 +11,7 @@ var autocomp_dest;
 var autocomp_stop;
 var origin_place;
 var dest_place;
-var stop_place;
+var stop_keyword;
 
 // Direction service variables
 var directionsService;
@@ -19,6 +19,9 @@ var directionsDisplay;
 
 // Variables that will store path information
 var pathPoints;
+var stopCandidates;
+
+var infowindow;
 
 // Initializes the map area and assigns autocomplete functions to text boxes
 function initMap() {
@@ -38,22 +41,26 @@ function initMap() {
 	autocomp_dest = new google.maps.places.Autocomplete(
 		(document.getElementById('dest')),
 		{types: []});
-		
-	// Add autocomplete to stopoff textbox
-	autocomp_stop = new google.maps.places.Autocomplete(
-		(document.getElementById('stoploc')),
-		{types: []});
-
-	// Set the autocomplete to bias the current map area
-	autocomp_origin.bindTo('bounds', map);
-	autocomp_dest.bindTo('bounds', map);
-	autocomp_stop.bindTo('bounds', map);
+	
+	// Add autocomplete to stop off location textbox
+	autocomp_stop = new google.maps.places.SearchBox(document.getElementById('stoploc'));
+	
+	// Add listener to set the autocomplete bias when the maps viewport changes
+	map.addListener('bounds_changed', function() {
+		autocomp_origin.bindTo('bounds', map);
+		autocomp_dest.bindTo('bounds', map);
+		autocomp_stop.bindTo('bounds', map);
+	});
 
 	// Initialize the direction service objects
 	directionsService = new google.maps.DirectionsService;
 	directionsDisplay = new google.maps.DirectionsRenderer;
 	
+	// Apply handler to the Find Route button
 	document.getElementById('findRoute').onclick = findRoute;
+	document.getElementById('findStop').onclick = findStopOff;
+	
+	infowindow = new google.maps.InfoWindow();
 }
 
 // Retrieves the origin and destination points from the textboxes and displays the route on the map
@@ -81,7 +88,7 @@ function findRoute() {
 		}
 	});
 	
-	// Display the section to enter stop-off location
+	// Display the section on the webpage to enter a stop-off location
 	document.getElementById('stopsec').style.display = 'block';
 }
 
@@ -107,6 +114,7 @@ function getPointsOnPath(result) {
 		pathPoints.push(result.routes[0].overview_path[result.routes[0].overview_path.length - 1]);
 	}
 
+	/*
 	// Display markers (for testing purposes)
 	for(index = 0; index < pathPoints.length; ++index) {
 		marker = new google.maps.Marker({
@@ -114,4 +122,61 @@ function getPointsOnPath(result) {
 			map: map,
 		});
 	}
+	*/
+}
+
+// Function that searches all of the points along the path for potential stop off locations
+function findStopOff() {
+	var index;
+	
+		// Display the section on the webpage to select the suggested stop off location
+	document.getElementById('stopsel').style.display = 'block';
+	
+	// Clear out array that stores the potential stop locations
+	stopCandidates = [];
+	
+	// Retrieve the search keyword from the textbox
+	stop_keyword = document.getElementById('stoploc').value;
+	
+	// Create a place service variable
+	var service = new google.maps.places.PlacesService(map);
+	
+	/*
+	// Loop through every point along the path and perform a search
+	for(index = 0; index < pathPoints.length; ++index) {
+		service.textSearch({
+			location: pathPoints[index],
+			radius: searchRadius,
+			query: stop_keyword,
+			rankBy: google.maps.places.RankBy.DISTANCE
+		}, addPoints); // Points are added to the candidate array using this callback function
+	}
+	*/
+}
+
+// Function that takes the result of a text search and computes the total distance of the trip
+// that passes through each point that is returned
+function addPoints(results, status) {
+	// Check status of the text search
+	if (status === google.maps.places.PlacesServiceStatus.OK) {
+		// Loop through each result of the search and adds an element to the candidate array
+		for (var i = 0; i < results.length; i++) {
+			
+		}
+	}
+	else {
+		alert(status);
+	}
+}
+
+function createMarker(place) {
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location
+	});
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(place.name);
+		infowindow.open(map, this);
+	});
 }
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 A Google Maps extension that will allow users to find a convenient location to stop at while in route to their final destination.
 
 11/01/2015 - Loaded the files that we have so far to the repository. Currently, the website is capable of displaying the path once the user has entered an origin and destination. It also plots markers along the path that we will use to search for nearby stop-off locations and optimize the route.
+11/15/2015 - Uploaded additional code that is working towards completing the main algorithm that finds the optimal stop-off locations.
 
 <b>MainScreen.html</b> - The main html file for the website. It contains the necessary templates to house the map and all of the necessary controls. <br/>
-<b>MainScreen.css</b> - A supplemental CSS file that provides additional styling to customize the website. The website makes use of some of the features of the Bootstrap framework, but it does not follow the full ideaology of the framework. <br/>
-<b>InitMap.js</b> - The javascript file that loads the map and other controls that utilize the Google Maps API. Also contains all the other javascript to test out various pieces of the API. Some of the functions should be places into additional javascript files for convenience. <br/>
+<b>MainScreen.css</b> - A supplemental CSS file that provides additional styling to customize the website. The website makes use of some of the features of the Bootstrap framework, but it does not follow the full ideology of the framework. <br/>
+<b>InitMap.js</b> - The javascript file that loads the map and other controls that utilize the Google Maps API. Also contains all the other javascript to test out various pieces of the API. Some of the functions should be placed into additional javascript files for convenience. <br/>
